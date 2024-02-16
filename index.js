a3a7a2589c656ae06d21109f5f576a8da97ab68f
Sameer Kumar <sam210594@gmail.com>
2024-02-17 00:56:14 +0530

    added cors

--- index.js
+++ index.js
@@ -2,12 +2,13 @@ require('dotenv').config();
 const mongoose = require('mongoose')
 const express = require('express');
 const nodemailer = require("nodemailer");
-
+const cors = require('cors')
 
 const app = express()
 
 const port = 5000
 app.use(express.json())
+app.use(cors())
 
 app.get('/', (req, res) => {
     res.send('hello')
